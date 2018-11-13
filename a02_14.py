adf84725b1a17ed0ffbce9a922d75cd86c54fce0
Paulo Campos Zava <camposzava@gmail.com>
2018-11-13 15:38:07 +0100

    pytorch

---
+++ a02_14.py
@@ -0,0 +1,11 @@
+e = 0.577215664901
+
+f = lambda x,y : 4*x + 5*y - 9
+
+sigma = lambda x,y: 1 / (1 + e**(-1 * f(x,y)))
+softmax = lambda x, y: (e ** x) / ((e**x) + (e**y))
+
+sigma(1,5)
+softmax(1,5)
+
+
