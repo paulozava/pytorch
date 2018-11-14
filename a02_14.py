6bf0690b560cac897e632acea271e4e4b13e4275
Paulo Campos Zava <camposzava@gmail.com>
2018-11-14 11:31:36 +0100

    pytorch

--- a02_14.py
+++ a02_14.py
@@ -1,11 +1,17 @@
 from numpy import e
+from numpy import log
 
 f = lambda x,y : 4*x + 5*y - 9
 
 sigma = lambda x,y: 1 / (1 + e**(-1 * f(x,y)))
 softmax = lambda x, y: (e ** x) / ((e**x) + (e**y))
 
+def cross_entropy(ylist, plist):
+    result = []
+    for p, y in zip(plist, ylist):
+        ce = y * log(p) + (1 - y) * log(1 - p)
+        result.append(ce)
+    return result
+
 sigma(1,5)
 softmax(1,5)
-
-
